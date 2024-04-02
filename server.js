e0e73c9cbffffd070adb126beba338e558bcfc39
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-04-02 10:55:08 +0000

    database setup

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 app = express();
+const mongoose = require('mongoose');
 const dotenv = require ('dontenv')
 
 
 app.use(express.json())
+const port = process.env.PORT 
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/cordCtrl')
+.then(()=>{
+    console.log('connected to mongodb');
+    app.listen(port, ()=> console.log(`Server connect to port ${port}`));
+})
+.catch(()=>{
+    console.error("unable to coonect to mongodb");
+})
+
+
+
+
 
 
 
