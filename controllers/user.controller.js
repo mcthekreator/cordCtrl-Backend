34316e06751a8c97f3eee2d8b45557564d2989e4
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-04-22 20:21:32 +0000

    registe model

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,33 +1,43 @@
 const userModel = require("../model/users.model");
-const bcrypt = require ('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userController = {
   registerUser: async (req, res) => {
     try {
-        const { username, email, password,image,address1,address2,region,country,verificationCode } = req.body;
-        const salt = await bcrypt.genSalt(15);
-        const hashpassword = await bcrypt.hash(password, salt);
-        const newUser = new userModel({
-            username,
-            email,
-            password: hashpassword,
-            image,
-            address1,
-            address2,
-            region,
-            country,
-            verificationCode,
-        })
-        await newUser.save()
-        let payload = {subject: newUser.subject}
-        let token = jwt.sign(payload, process.env.JWT_SECRET)
-        res.status(200).send({token})
+      const {
+        name,
+        email,
+        password,
+        image,
+        address1,
+        address2,
+        region,
+        country,
+        verificationCode,
+      } = req.body;
+      const salt = await bcrypt.genSalt(15);
+      const hashpassword = await bcrypt.hash(password, salt);
+      const newUser = new userModel({
+        name,
+        email,
+        password: hashpassword,
+        image,
+        address1,
+        address2,
+        region,
+        country,
+        verificationCode,
+      });
+      await newUser.save();
+      let payload = { subject: newUser.subject };
+      let token = jwt.sign(payload, process.env.JWT_SECRET);
+      res.status(200).send({ token });
     } catch (error) {
-        res.status(404).json({message:error.message})
+      res.status(404).json({ message: error.message });
     }
   },
-  login: async  (req, res)=>{
+  login: async (req, res) => {
     try {
       const user = await userModel.findOne({ email: req.body.email });
       if (!user) {
@@ -37,13 +47,13 @@ const userController = {
         req.body.password,
         user.password
       );
-  
+
       if (!validPassword) {
-        res.status(403).json({error: "Invalid password!"});
+        res.status(403).json({ error: "Invalid password!" });
       } else {
-        let payload = {subject: user.email}
-        token = jwt.sign(payload, process.env.JWT_SECRET)
-        res.status(200).json({token});
+        let payload = { subject: user.email };
+        token = jwt.sign(payload, process.env.JWT_SECRET);
+        res.status(200).json({ token });
       }
     } catch (error) {
       res.status(500).json(error);
