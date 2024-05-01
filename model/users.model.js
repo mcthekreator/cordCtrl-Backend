8822bce4af19a229f0ddb9fab768c09362e0c525
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-05-01 22:02:23 +0000

    changes with the db model

--- model/users.model.js
+++ model/users.model.js
@@ -6,11 +6,6 @@ const userModel = mongoose.Schema(
         name: { type: String, require: true },
         password: { type: String, require: true },
         email: { type: String, require: true, unique: true},
-        address1: { type: String, require: true},
-        address2: { type: String, require: true},
-        region: { type: String, require: true},
-        country: { type: String, require: true},
-        verificationCode: { type: String, require: true},
         image: { type: String, require: false },
      },
     
